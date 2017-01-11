22683e7d062a6e042b5deda10dba6a1be127cfd2
Evan Walsh <evan@massivedanger.com>
2017-01-11 18:06:06 -0500

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Evan Walsh's Node Boilerplate
+
+I've spent too much time messing around with Node stuff finding a setup that I like.
+
+This is what I like. I'm putting it on GitHub to save myself time in the future.
+
+You're welcome, Future Evan.
+
+❤️, Past Evan
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }          
+    ]
+  }
+};
